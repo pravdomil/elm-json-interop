bcff8107bd5e4ccefbec67fea88477c192126920
pravdomil <pravdomil.toman@gmail.com>
2020-02-03 18:05:47 +0100

    fix write

--- bin/fromPath.js
+++ bin/fromPath.js
@@ -33,5 +33,5 @@ const convert = a => {
 }
 
 main(process.argv.slice(2))
-  .then(a => process.stdout.write(a))
-  .catch(a => process.stderr.write(a))
+  .then(a => process.stdout.write(String(a)))
+  .catch(a => process.stderr.write(String(a)))
