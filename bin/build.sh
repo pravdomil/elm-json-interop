bef9d0aca96acdc9c3bd23a1facd0af1b7ec18a6
pravdomil <pravdomil.toman@gmail.com>
2020-08-24 13:51:58 +0200

    new build system

--- bin/build.sh
+++ bin/build.sh
@@ -10,4 +10,14 @@ set -u
 cd "${0%/*}/.."
 
 # To build the application.
-elm make src/Main.elm --output dist/main.js --optimize
+elm make src/Main.elm --output bin/elm-json-interop.js --optimize
+(
+  echo "#!/usr/bin/env node"
+  cat bin/elm-json-interop.js |
+    sed -E "s/(var \\\$author\\\$project\\\$Main\\\$eval = .*)/\1eval(a);/g"
+  echo ""
+  echo "const stdin = process.stdin.isTTY ? \"\" : require(\"fs\").readFileSync(0, \"utf-8\");"
+  echo "this.Elm.Main.init({ flags: { argv: process.argv, stdin: stdin } } );"
+) >bin/elm-json-interop
+rm bin/elm-json-interop.js
+chmod +x bin/elm-json-interop
