15e8b712a05240a710bc2c2ea53a79dac10afa14
pravdomil <pravdomil.toman@gmail.com>
2020-11-04 15:33:39 +0100

    taskAndThen

--- src/Eval.elm
+++ src/Eval.elm
@@ -153,3 +153,75 @@ copyFile source destination =
 toString : String -> String
 toString a =
     a |> Encode.string |> encode 0
+
+
+
+--
+
+
+{-| -}
+taskAndThen2 : (a -> b -> Task x result) -> Task x a -> Task x b -> Task x result
+taskAndThen2 func taskA taskB =
+    taskA
+        |> Task.andThen
+            (\a ->
+                taskB
+                    |> Task.andThen (\b -> func a b)
+            )
+
+
+{-| -}
+taskAndThen3 : (a -> b -> c -> Task x result) -> Task x a -> Task x b -> Task x c -> Task x result
+taskAndThen3 func taskA taskB taskC =
+    taskA
+        |> Task.andThen
+            (\a ->
+                taskB
+                    |> Task.andThen
+                        (\b ->
+                            taskC
+                                |> Task.andThen (\c -> func a b c)
+                        )
+            )
+
+
+{-| -}
+taskAndThen4 : (a -> b -> c -> d -> Task x result) -> Task x a -> Task x b -> Task x c -> Task x d -> Task x result
+taskAndThen4 func taskA taskB taskC taskD =
+    taskA
+        |> Task.andThen
+            (\a ->
+                taskB
+                    |> Task.andThen
+                        (\b ->
+                            taskC
+                                |> Task.andThen
+                                    (\c ->
+                                        taskD
+                                            |> Task.andThen (\d -> func a b c d)
+                                    )
+                        )
+            )
+
+
+{-| -}
+taskAndThen5 : (a -> b -> c -> d -> e -> Task x result) -> Task x a -> Task x b -> Task x c -> Task x d -> Task x e -> Task x result
+taskAndThen5 func taskA taskB taskC taskD taskE =
+    taskA
+        |> Task.andThen
+            (\a ->
+                taskB
+                    |> Task.andThen
+                        (\b ->
+                            taskC
+                                |> Task.andThen
+                                    (\c ->
+                                        taskD
+                                            |> Task.andThen
+                                                (\d ->
+                                                    taskE
+                                                        |> Task.andThen (\e -> func a b c d e)
+                                                )
+                                    )
+                        )
+            )
