6428e5c2690e66c8d0560df07f5449c44d3f3fe6
pravdomil <pravdomil.toman@gmail.com>
2020-11-30 18:54:52 +0100

    Test.elm

---
+++ example/src/Test.elm
@@ -0,0 +1,27 @@
+module Test exposing (..)
+
+import Generated.Msg.Decode as MsgDecode
+import Generated.Msg.Encode as MsgEncode
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Msg exposing (Msg(..))
+import User exposing (User(..))
+
+
+{-| -}
+msg : Msg
+msg =
+    ReceivedMessages
+        [ { user = Visitor "Paul", message = Nothing }
+        ]
+
+
+{-| -}
+test : Bool
+test =
+    msg
+        |> MsgEncode.msg
+        |> Encode.encode 0
+        |> Decode.decodeString MsgDecode.msg
+        |> Result.map ((==) msg)
+        |> Result.withDefault False
