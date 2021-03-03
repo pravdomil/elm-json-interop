23f57ed85176461c213525a87489a554448ffd8f
pravdomil <pravdomil.toman@gmail.com>
2021-03-03 23:07:55 +0100

    format

--- tests/Msg/Decode.elm
+++ tests/Msg/Decode.elm
@@ -34,4 +34,37 @@ msg =
 
 
 message aDecoder bDecoder =
-    D_.map15 (\a b c d e f g h i j k l m n o -> { bool = a, int = b, float = c, char = d, string = e, unit = f, tuple = g, tuple3 = h, list = i, array = j, record = k, maybe = l, result = m, set = n, dict = o }) (D.field "bool" D.bool) (D.field "int" D.int) (D.field "float" D.float) (D.field "char" D_.char) (D.field "string" D.string) (D.field "unit" D_.unit) (D.field "tuple" (D_.tuple aDecoder bDecoder)) (D.field "tuple3" (D_.tuple3 aDecoder bDecoder bDecoder)) (D.field "list" (D.list (D.map2 (\a b -> { a = a, b = b }) (D.field "a" aDecoder) (D.field "b" bDecoder)))) (D.field "array" (D.array (D.map2 (\a b -> { a = a, b = b }) (D.field "a" aDecoder) (D.field "b" bDecoder)))) (D.field "record" (D.succeed {})) (D_.maybeField "maybe" (D_.maybe aDecoder)) (D.field "result" (D_.result D.int aDecoder)) (D.field "set" (D_.set D.int)) (D.field "dict" (D_.dict D.int aDecoder))
+    D_.map15
+        (\a b c d e f g h i j k l m n o ->
+            { bool = a
+            , int = b
+            , float = c
+            , char = d
+            , string = e
+            , unit = f
+            , tuple = g
+            , tuple3 = h
+            , list = i
+            , array = j
+            , record = k
+            , maybe = l
+            , result = m
+            , set = n
+            , dict = o
+            }
+        )
+        (D.field "bool" D.bool)
+        (D.field "int" D.int)
+        (D.field "float" D.float)
+        (D.field "char" D_.char)
+        (D.field "string" D.string)
+        (D.field "unit" D_.unit)
+        (D.field "tuple" (D_.tuple aDecoder bDecoder))
+        (D.field "tuple3" (D_.tuple3 aDecoder bDecoder bDecoder))
+        (D.field "list" (D.list (D.map2 (\a b -> { a = a, b = b }) (D.field "a" aDecoder) (D.field "b" bDecoder))))
+        (D.field "array" (D.array (D.map2 (\a b -> { a = a, b = b }) (D.field "a" aDecoder) (D.field "b" bDecoder))))
+        (D.field "record" (D.succeed {}))
+        (D_.maybeField "maybe" (D_.maybe aDecoder))
+        (D.field "result" (D_.result D.int aDecoder))
+        (D.field "set" (D_.set D.int))
+        (D.field "dict" (D_.dict D.int aDecoder))
