be2ac918a25351cbbc9a71d655a37e1b864e3418
pravdomil <pravdomil.toman@gmail.com>
2021-03-05 10:45:51 +0100

    empty dependencies module

--- src/Generators/Dependencies.elm
+++ src/Generators/Dependencies.elm
@@ -1,59 +1,7 @@
 module Generators.Dependencies exposing (..)
 
 import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Expression exposing (Expression(..), Function, LetDeclaration(..))
 import Elm.Syntax.File exposing (File)
 import Elm.Syntax.ModuleName exposing (ModuleName)
 import Elm.Syntax.Node as Node exposing (Node)
-import Elm.Syntax.TypeAnnotation exposing (TypeAnnotation(..))
-
-
-fromFile : File -> List ( ModuleName, String )
-fromFile a =
-    a.declarations |> List.concatMap fromDeclaration
-
-
-fromDeclaration : Node Declaration -> List ( ModuleName, String )
-fromDeclaration a =
-    case Node.value a of
-        FunctionDeclaration _ ->
-            []
-
-        AliasDeclaration b ->
-            b.typeAnnotation |> fromTypeAnnotation
-
-        CustomTypeDeclaration b ->
-            b.constructors |> List.concatMap (Node.value >> .arguments >> List.concatMap fromTypeAnnotation)
-
-        PortDeclaration _ ->
-            []
-
-        InfixDeclaration _ ->
-            []
-
-        Destructuring _ _ ->
-            []
-
-
-fromTypeAnnotation : Node TypeAnnotation -> List ( ModuleName, String )
-fromTypeAnnotation a =
-    case Node.value a of
-        GenericType _ ->
-            []
-
-        Typed b c ->
-            (b |> Node.value) :: (c |> List.concatMap fromTypeAnnotation)
-
-        Unit ->
-            []
-
-        Tupled b ->
-            b |> List.concatMap fromTypeAnnotation
-
-        Record b ->
-            b |> List.concatMap (Node.value >> Tuple.second >> fromTypeAnnotation)
-
-        GenericRecord _ _ ->
-            []
-
-        FunctionTypeAnnotation _ _ ->
-            []
